addcee2cc20ef5d04aff4a1fea3321d850e0e9f5
BewareMyPower <xyzinfernity@163.com>
2018-07-23 23:19:59 +0800

    删除多余头文件

--- examples/oob-client.cc
+++ examples/oob-client.cc
@@ -1,7 +1,6 @@
 // oob-client.cc: 发送带外数据
 #include "inet_socket.h"
 #include "util/errors.hpp"
-#include <sys/select.h>
 
 using namespace socket_util;
 using namespace socket_util::util;
@@ -9,7 +8,6 @@ using namespace socket_util::util;
 int main(int argc, char* argv[]) {
     int sockfd = inet::createTcpClient({"127.0.0.1", 8888});
 
-    // 依次发送普通数据、带外数据、普通数据
     inet::sendAll(sockfd, "123");
     inet::sendAll(sockfd, "abc", MSG_OOB);
     inet::sendAll(sockfd, "123");

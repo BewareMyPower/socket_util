263de5d7075f8ee3b984d5bd115cfe3e20e51fae
BewareMyPower <xyzinfernity@163.com>
2018-08-22 15:43:51 +0800

    将readNBytes函数签名改成和sendNBytes一致，将返回字节数的函数独立出来， 作为readAsMuchAsPossible，以用于非阻塞描述符的读取。

--- socket_util/io_util.cc
+++ socket_util/io_util.cc
@@ -26,8 +26,13 @@ bool writeAll(WriteFunc writeFunc, int fd, const void* buf, size_t n) noexcept {
     return true;
 }
 
-ssize_t readNBytes(ReadFunc readFunc, int fd, void* buf, size_t n) noexcept {
-    size_t num_left = n;
+bool readNBytes(ReadFunc readFunc, int fd, void* buf, size_t n) noexcept {
+    ssize_t num_read = readAsMuchAsPossible(readFunc, fd, buf, n);
+    return num_read == static_cast<ssize_t>(n);
+}
+
+ssize_t readAsMuchAsPossible(ReadFunc readFunc, int fd, void* buf, size_t bufsize) noexcept {
+    auto num_left = static_cast<ssize_t>(bufsize);
     auto ptr = static_cast<char*>(buf);
 
     while (num_left > 0) {
@@ -41,13 +46,14 @@ ssize_t readNBytes(ReadFunc readFunc, int fd, void* buf, size_t n) noexcept {
                 return -1;
             }
         } else if (num_read == 0) {
-            break;  // EOF
+            break;  // 读到EOF
         }
 
         num_left -= num_read;
         ptr += num_read;
     }
-    return n - num_left;
+
+    return static_cast<ssize_t>(bufsize) - num_left;
 }
 
 }  // END namespace io_util
--- socket_util/io_util.h
+++ socket_util/io_util.h
@@ -32,11 +32,23 @@ bool writeAll(WriteFunc writeFunc, int fd, const void* buf, size_t n) noexcept;
  *   buf       指向缓冲区首地址
  *   n         读取的字节数
  * 返回值:
- *   读取的字节数量，若读取失败则返回-1，errno被设置
- * 说明:
- *   正常情况下返回值为n，若中途读取到EOF则返回值会小于n。
+ *   若全部读取成功则返回true，否则返回false，errno被设置
  */
-ssize_t readNBytes(ReadFunc readFunc, int fd, void* buf, size_t n) noexcept;
+bool readNBytes(ReadFunc readFunc, int fd, void* buf, size_t n) noexcept;
+
+/**
+ * 功能: 从文件描述符中尽可能地读取字节
+ * 参数:
+ *   readFunc  读取函数
+ *   fd        待读取的文件描述符
+ *   buf       指向缓冲区首地址
+ *   bufsize   缓冲区大小，即最多读取的字节数
+ * 返回值:
+ *   成功读取的字节数，若读取失败则返回-1，errno被设置
+ * 说明:
+ *   fd应设为非阻塞模式，否则在可读字节数小于count时会导致阻塞。
+ */
+ssize_t readAsMuchAsPossible(ReadFunc readFunc, int fd, void* buf, size_t bufsize) noexcept;
 
 }  // END namespace io_util
 
